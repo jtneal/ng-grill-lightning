a87f0ae7fbd01cb11d41f2d5c87dc9f7daf64b0a
Jason Neal <jasonneal1@quickenloans.com>
2021-04-20 10:32:42 -0400

    ng add ng-micro-frontend --project order --type micro --port 4220

--- projects/order/src/app/app.module.ts
+++ projects/order/src/app/app.module.ts
@@ -1,8 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ApplicationRef, DoBootstrap, Injector, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { createCustomElement } from '@angular/elements';
+import { environment } from '../environments/environment';
+import { MicroFrontendModule } from 'ng-micro-frontend';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,23 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MicroFrontendModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule implements DoBootstrap { 
+  public constructor(
+    private readonly injector: Injector
+  ) { }
+
+  public ngDoBootstrap(appRef: ApplicationRef): void {
+    if (environment.production) {
+      const customElement = createCustomElement(AppComponent, { injector: this.injector });
+      customElements.define('custom-order', customElement);
+    } else {
+      appRef.bootstrap(AppComponent);
+    }
+  }
+
+}
