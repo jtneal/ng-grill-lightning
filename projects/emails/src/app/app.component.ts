f9642a7ba171fb5a8b6b2f28d7de5706096ebe37
Jason Neal <jasonneal1@quickenloans.com>
2021-04-20 11:15:21 -0400

    ng add ng-micro-frontend --project emails --type micro --port 4240

--- projects/emails/src/app/app.component.ts
+++ projects/emails/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  public constructor(
+    private readonly location: Location,
+    private readonly router: Router
+  ) { }
+
+  public ngOnInit(): void {
+    if (environment.production) {
+      this.router.initialNavigation();
+      this.router.navigate([this.location.path()]);
+    }
+  }
+
   title = 'emails';
 }
---
+++ projects/emails/webpack.config.js
@@ -0,0 +1,10 @@
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+
+module.exports = {
+  plugins: [
+    new WebpackManifestPlugin({
+      filter: (file) => file.name.endsWith('.js') || file.name.endsWith('.css'),
+      seed: { customElement: 'custom-emails' },
+    }),
+  ],
+};
