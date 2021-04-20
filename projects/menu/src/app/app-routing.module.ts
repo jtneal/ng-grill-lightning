634fb782f21e15c55a6357b8c1283f18c14d92ef
Jason Neal <jasonneal1@quickenloans.com>
2021-04-20 10:52:23 -0400

    Add menu routes

--- projects/menu/src/app/app-routing.module.ts
+++ projects/menu/src/app/app-routing.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NullComponent } from 'ng-micro-frontend';
 
-const mfeRoutes: Routes = [];
+import { BreakfastComponent } from './breakfast/breakfast.component';
+import { DinnerComponent } from './dinner/dinner.component';
+import { LunchComponent } from './lunch/lunch.component';
 
-const routes: Routes = [{ children: mfeRoutes, path: 'menu' },
-  { component: NullComponent, path: '**' }];
+const mfeRoutes: Routes = [
+  { component: BreakfastComponent, path: 'breakfast' },
+  { component: LunchComponent, path: 'lunch' },
+  { component: DinnerComponent, path: 'dinner' },
+];
+
+const routes: Routes = [
+  { children: mfeRoutes, path: 'menu' },
+  { component: NullComponent, path: '**' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
